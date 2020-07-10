ef6bfbf3a80136f1e9dc1d432a9148e475d80057
王嘉炀 <346461062@qq.com>
2020-07-11 00:23:16 +0800

    update

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -4,32 +4,18 @@ import BreadcrumbView from "./breadcrumb";
 
 const PageHeader = (props) => {
   console.log("PageHeader", props);
-  const {
-    title,
-    logo,
-    action,
-    content,
-    extraContent,
-    hiddenBreadcrumb = false,
-  } = props;
+  const { title, content, hiddenBreadcrumb = false } = props;
   return (
     <div className={styles.pageHeader}>
       {hiddenBreadcrumb ? null : <BreadcrumbView {...props} />}
-      <div className={styles.detail}>
-        {logo && <div className={styles.logo}>{logo}</div>}
-        <div className={styles.main}>
-          <div className={styles.row}>
-            {title && <h1 className={styles.title}>{title}</h1>}
-            {action && <div className={styles.action}>{action}</div>}
-          </div>
-          <div className={styles.row}>
-            {content && <div className={styles.content}>{content}</div>}
-            {extraContent && (
-              <div className={styles.extraContent}>{extraContent}</div>
-            )}
+      {title && (
+        <div className={styles.heading}>
+          <div className={styles.headingLeft}>
+            <span className={styles.headingTitle}>{title}</span>
           </div>
         </div>
-      </div>
+      )}
+      {content && <div className="headingContent">{content}</div>}
     </div>
   );
 };
